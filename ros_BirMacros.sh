568bc6df6eff47cd0f1315281a78fdb538f398f9
Caio Amaral <caioaamaral@gmail.com>
2019-04-30 13:30:17 -0300

    add --code flagto setEnvironment

--- ros_BirMacros.sh
+++ ros_BirMacros.sh
@@ -8,6 +8,9 @@ setEnvironment () {
     source ~/.bashrc
     source $WORKSPACE_PATH/$WORKSPACE_NAME/devel/setup.bash
     cd $WORKSPACE_PATH/$WORKSPACE_NAME
+    if [[ $2 = --code ]]; then
+        code $WORKSPACE_PATH/$WORKSPACE_NAME
+    fi
 }
 
 workspace(){
@@ -25,3 +28,7 @@ workspace(){
 alias jacob_ws='setEnvironment jacob_ws'
 alias kinova_ws='setEnvironment kinova_ws'
 alias dyn_ws='setEnvironment dyn_ws'
+alias pt_ws='setEnvironment pt_ws'
+alias kinova_b='setEnvironment kinova_b'
+alias pybullet='setEnvironment pybullet'
+alias pybullet_ws='setEnvironment pybullet_ws'
